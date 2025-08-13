cd1dd73be1afd22436eb883aa091f68862cd0732
bknthejane <159468648+bknthejane@users.noreply.github.com>
2025-08-13 10:13:59 +0200

    Initial commit!

---
+++ backend/src/bonolo.shesha.Web.Core/sheshaWebCoreModule.cs
@@ -0,0 +1,87 @@
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Castle.MicroKernel.Registration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using bonolo.shesha.Application;
+using bonolo.shesha.Common.Authorization;
+using bonolo.shesha.Domain;
+using Shesha;
+using Shesha.Authentication.JwtBearer;
+using Shesha.Authorization;
+using Shesha.Configuration;
+using Shesha.Configuration.Startup;
+using Shesha.Import;
+using Shesha.Sms.Clickatell;
+using Shesha.Web.FormsDesigner;
+using System;
+using System.Text;
+
+namespace bonolo.shesha
+{
+    /// <summary>
+    /// ReSharper disable once InconsistentNaming
+    /// </summary>
+    [DependsOn(
+        // Adding all the shesha Modules
+        typeof(SheshaFrameworkModule),
+        typeof(SheshaApplicationModule),
+        typeof(SheshaFormsDesignerModule),
+        typeof(SheshaImportModule),
+        typeof(SheshaClickatellModule),
+        typeof(sheshaModule),
+        typeof(sheshaApplicationModule)
+	 )]
+    public class sheshaWebCoreModule : AbpModule
+    {
+        private readonly IConfigurationRoot _appConfiguration;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="env"></param>
+        public sheshaWebCoreModule(IWebHostEnvironment env)
+        {
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void PreInitialize()
+        {
+            var config = Configuration.Modules.ShaNHibernate();
+            
+            config.UseDbms(c => c.GetDbmsType(), c => c.GetDefaultConnectionString());
+
+            //config.UseMsSql();
+            //config.UsePostgreSql();
+
+            ConfigureTokenAuth();
+        }
+
+        private void ConfigureTokenAuth()
+        {
+            IocManager.Register<TokenAuthConfiguration>();
+            var tokenAuthConfig = IocManager.Resolve<TokenAuthConfiguration>();
+
+            tokenAuthConfig.SecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appConfiguration["Authentication:JwtBearer:SecurityKey"]));
+            tokenAuthConfig.Issuer = _appConfiguration["Authentication:JwtBearer:Issuer"];
+            tokenAuthConfig.Audience = _appConfiguration["Authentication:JwtBearer:Audience"];
+            tokenAuthConfig.SigningCredentials = new SigningCredentials(tokenAuthConfig.SecurityKey, SecurityAlgorithms.HmacSha256);
+            tokenAuthConfig.Expiration = TimeSpan.FromDays(5);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(sheshaWebCoreModule).GetAssembly());
+
+            IocManager.IocContainer.Register(
+            Component.For<ICustomPermissionChecker>().Forward<IsheshaPermissionChecker>().Forward<sheshaPermissionChecker>().ImplementedBy<sheshaPermissionChecker>().LifestyleTransient()                );
+        }
+    }
+}
