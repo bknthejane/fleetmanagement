52f5a7daa3482ae5c2ec9cca42f1516c3d11aa1a
Bonolo-Nth <bknthejane@gmail.com>
2025-08-14 15:07:28 +0200

    Created the Municipality Endpoint

---
+++ backend/src/Module/bonolo.shesha.Domain/Domain/Municipality.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonolo.shesha.Domain.Domain
+{
+    public class Municipality : FullAuditedEntity<Guid>
+    {
+        public virtual string Name { get; set; }
+        public virtual string Type { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string ContactPerson { get; set; }
+        public virtual string Email {  get; set; }
+        public virtual string ContactNumber { get; set; }
+    }
+}
