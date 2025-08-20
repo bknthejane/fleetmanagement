a9295184905ce6e9bcc50827e02f6c5df2448d75
Bonolo-Nth <bknthejane@gmail.com>
2025-08-20 18:40:12 +0200

    Implemented Supervisor Creation

---
+++ backend/src/Module/bonolo.shesha.Domain/Domain/Supervisor.cs
@@ -0,0 +1,13 @@
+﻿using Shesha.Domain;
+using Shesha.Domain.Attributes;
+using System;
+
+namespace bonolo.shesha.Domain.Domain
+{
+    [Entity(TypeShortAlias = "shesha.Supervisor")]
+    public class Supervisor : Person
+    {
+        public virtual string Department { get; set; }
+        public virtual Municipality Municipality { get; set; }
+    }
+}
